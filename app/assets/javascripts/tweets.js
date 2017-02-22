bad687f1285376fe10efe296ba7f102b7f9040a6
Richard Strauss <rstrauss91@gmail.com>
2017-02-22 09:28:44 -0500

    create html javascript logic

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+
+  $('#new_tweet').submit(function(event) { //the id selector needs to be on the form itself
+    event.preventDefault();
+    console.log($(this).serialize());
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: $(this).serialize(),
+      dataType: 'html',
+    }).done(function(responseData) {
+      console.log('successfully completed');
+      $(responseData).prependTo($('.tweets'));
+    }).fail(function(responseData){
+      console.log('failed');
+    }).always(function(responseData) {
+      console.log('this should always be logged back')
+    })
+    })
+  })
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,11 +7,16 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
 
-    if @tweet.save
-      redirect_to tweets_path
-    else
-      render :index
+    respond_to do |format|
+      @tweet.save
+      format.html { render html: "<li class='tweet'><p>#{@tweet.message}</p><time>#{@tweet.created_at.strftime('%b %e, %l:%M %p')}</time><li>".html_safe }
     end
+
+    # if @tweet.save
+    #   redirect_to tweets_path
+    # else
+    #   render :index
+    # end
   end
 
   def destroy
